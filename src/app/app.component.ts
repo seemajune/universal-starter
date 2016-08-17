be2033377c5c02c8713447176a7783e67e01a377
Seema Shariat <sshariat@750-MAC-AFVH8.local>
2016-08-17 16:29:57 -0400

    edits to style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,13 +41,13 @@ export class About { }
     XLarge
   ],
   styles: [`
-    * { padding:0; margin:0; }
+    * { padding:0; margin:0; font-family:Helvetica; }
     #universal { text-align:center; font-weight:bold; padding:15px 0; }
-    nav { background:#158126; min-height:40px; border-bottom:5px #046923 solid; }
+    nav { background:#d44d5c; min-height:40px; border-bottom:5px #000 solid; }
     nav a { font-weight:bold; text-decoration:none; color:#fff; padding:20px; display:inline-block; }
-    nav a:hover { background:#00AF36; }
-    .router-link-active { background-color: #00AF36; }
-    blockquote { border-left:5px #158126 solid; background:#fff; padding:20px 20px 20px 40px; }
+    nav a:hover { background:#773344; }
+    .router-link-active { background-color: #e3b5a4; }
+    blockquote { border-left:5px #773344 solid; background:#fff; padding:20px 20px 20px 40px; }
     blockquote::before { left: 1em; }
     main { padding:20px 0; }
     pre { font-size:12px; }
@@ -55,14 +55,23 @@ export class About { }
   template: `
   <h3 id="universal">Angular2 Universal</h3>
   <nav>
-    <a [routerLinkActive]="['active', 'router-link-active']" [routerLink]=" ['./home'] ">{{ server }}</a>
+    <a [routerLinkActive]="['active', 'router-link-active']" [routerLink]=" ['./home'] ">Home</a>
   </nav>
   <div>
       <span x-large>{{ title }}!</span>
-      Two-way binding: <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
+
+      <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
       <br><br>
+
       <strong>Async data call return value:</strong>
       <pre>{{ data | json }}</pre>
+
+      <strong>Router-outlet:</strong>
+      <main>
+        <router-outlet></router-outlet>
+      </main>
+
+      <blockquote>{{ server }}</blockquote>
   </div>
   `
 })
